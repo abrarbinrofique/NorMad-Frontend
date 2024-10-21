896d12ca2763176f57c2bc3738d1301052c81ad1
abrarbinrofique <researchabrarrofique@gmail.com>
2024-10-22 03:51:00 +0600

    update landing page

--- index.js
+++ index.js
@@ -1,10 +1,91 @@
 let cus=localStorage.getItem('user_id')
 let t=localStorage.getItem('token');
 
+
+
+
+
+
+async function get(){
+      
+  let x= await fetch('https://normad-bakend.vercel.app/event/review/')
+  let data=await x.json()
+
+  const paren = document.getElementById('slider2')
+
+
+data.forEach((rv) => {
+const li=document.createElement('li')
+
+
+let customerid=rv.customer
+console.log(customerid)
+rvname(customerid)
+
+
+
+
+
+
+
+async function rvname(customerid){
+  const res =await  fetch(`https://homeper-backend.vercel.app/customer/${customerid}/`);
+    const data =await  res.json();
+
+   
+  let  name= data.user
+   
+
+
+   li.innerHTML = `
+       
+            <div class="card shadow w-20">
+              
+                   <img src="https://res.cloudinary.com/dk2vgd0dv/image/upload/v1723566992/samples/landscapes/nature-mountains.jpg" class="card-img-top" loading="lazy" alt="...">
+                 
+                <div class="card-body d-flex flex-column flex-md-row">
+                    <div class="flex-grow-1">
+                        <strong>${rv.textreview}</strong>
+                        <p>personal ratting:  ${rv.ratting}⭐</p>
+
+                        <p class="card-text">${name}</p>
+                    </div>
+                     
+                   
+                 
+                </div>
+                
+                
+                
+            </div>
+       
+
+
+               `
+   paren.appendChild(li)
+
+}
+
+
+})
+
+
+
+
+
+  
+    }
+
+  
+get()
+
+
      
 if (cus && t)
     {
     
+
+
   
   const ui=document.getElementById('profileid')
   
@@ -68,13 +149,13 @@ async function loadservice() {
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-md-7 col-12 order-md-1 order-2">
-                            <h4 class="p-5">${data.location}<br /></h4>
+                            <h4 class="m-3">${data.location}<br /></h4>
                             <p>${data.Description.slice(0, 250)}...More</p>
                             <h4><i class="fa-solid fa-calendar-days"></i> ${data.traveldate}</h4>
-                           <a href="Details.html?dataid=${data.id}" class="btn">Book Now</a>
+                           <a href="Details.html?dataid=${data.id}" class="btn btn-warning my-5">Book Now</a>
                         </div>
                         <div class="col-md-5 col-12 order-md-2 order-1">
-                            <img src="https://res.cloudinary.com/dk2vgd0dv/${data.image}" class="mx-auto img-fluid" alt="slide" />
+                            <img  src="https://res.cloudinary.com/dk2vgd0dv/${data.image}" class="mx-auto img-fluid" alt="slide" />
                         </div>
                     </div>
                 </div>
